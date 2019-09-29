08fe1e30d1a39a5bf9a47077da395ff2b5d39630
Shrawan_shakya <shrawanshaky3@gmail.com>
2019-09-29 19:27:42 +0545

    My commit

--- app/src/main/java/com/meroapp/myapp/MyCalculator.java
+++ app/src/main/java/com/meroapp/myapp/MyCalculator.java
@@ -13,51 +13,17 @@ import android.widget.Toast;
 public class MyCalculator extends AppCompatActivity implements View.OnClickListener {
 
     TextView txt;
-    Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnsum, btnsub, btnmulti, btndiv, btnper, btnequal;
-    String txtget, first, second, output;
-
+    Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnsum, btnsub, btnmulti, btndiv, btnper, btnequal, btnclear;
+    String txtget, first, second, output, result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_calculator);
 
-        txt = findViewById(R.id.txt);
-        btn0 = findViewById(R.id.btn0);
-        btn1 = findViewById(R.id.btn1);
-        btn2 = findViewById(R.id.btn2);
-        btn3 = findViewById(R.id.btn3);
-        btn4 = findViewById(R.id.btn4);
-        btn5 = findViewById(R.id.btn5);
-        btn6 = findViewById(R.id.btn6);
-        btn7 = findViewById(R.id.btn7);
-        btn8 = findViewById(R.id.btn8);
-        btn9 = findViewById(R.id.btn9);
-        btnsum = findViewById(R.id.btnsum);
-        btnsub = findViewById(R.id.btnsub);
-        btnmulti = findViewById(R.id.btnmulti);
-        btndiv = findViewById(R.id.btndiv);
-        btnper = findViewById(R.id.btnper);
-        btnequal = findViewById(R.id.btnequal);
-
-        btn0.setOnClickListener(this);
-        btn1.setOnClickListener(this);
-        btn2.setOnClickListener(this);
-        btn3.setOnClickListener(this);
-        btn4.setOnClickListener(this);
-        btn5.setOnClickListener(this);
-        btn6.setOnClickListener(this);
-        btn7.setOnClickListener(this);
-        btn8.setOnClickListener(this);
-        btn9.setOnClickListener(this);
-        btnsum.setOnClickListener(this);
-        btnsub.setOnClickListener(this);
-        btnmulti.setOnClickListener(this);
-        btndiv.setOnClickListener(this);
-        btnper.setOnClickListener(this);
-        btnequal.setOnClickListener(this);
+        button();
+        clickListener();
     }
-
 
     @Override
     public void onClick(View v) {
@@ -135,40 +101,83 @@ public class MyCalculator extends AppCompatActivity implements View.OnClickListe
             case R.id.btnequal:
                 if (txt.getText().toString() != null && !first.equals("")) {
                     second = txt.getText().toString();
-                    calculate(first, second, txtget);
-                    txt.setText(output);
-                    first = output;
+                    result = calculate(first, second, output);
+                    txt.setText(result);
                 }
+                break;
+
+            case R.id.btnclear:
+                txt.setText("");
                 break;
 
         }
     }
 
-    public void calculate(String first, String second, String output) {
+    public String calculate(String first, String second, String output) {
         switch (txtget) {
 
             case "Sum":
-                output = (Integer.parseInt(first) + Integer.parseInt(second)) + "";
+                output = (Double.parseDouble(first) + Double.parseDouble(second)) + "";
                 break;
 
             case "Substract":
-                output = (Integer.parseInt(first) - Integer.parseInt(second)) + "";
+                output = (Double.parseDouble(first) - Double.parseDouble(second)) + "";
                 break;
 
             case "Multiply":
-                output = (Integer.parseInt(first) * Integer.parseInt(second)) + "";
+                output = (Double.parseDouble(first) * Double.parseDouble(second)) + "";
                 break;
 
             case "Divide":
-                    output = (Double.parseDouble(first) / Double.parseDouble(second)) + "";
+                output = (Double.parseDouble(first) / Double.parseDouble(second)) + "";
                 break;
 
             case "Percentage":
                 output = (Double.parseDouble(first) / 100) + "";
                 break;
-
         }
-        Toast.makeText(this, "The result is " + output, Toast.LENGTH_LONG).show();
+        return output;
     }
 
+    public void button() {
+
+        txt = findViewById(R.id.txt);
+        btn0 = findViewById(R.id.btn0);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+        btn8 = findViewById(R.id.btn8);
+        btn9 = findViewById(R.id.btn9);
+        btnsum = findViewById(R.id.btnsum);
+        btnsub = findViewById(R.id.btnsub);
+        btnmulti = findViewById(R.id.btnmulti);
+        btndiv = findViewById(R.id.btndiv);
+        btnper = findViewById(R.id.btnper);
+        btnequal = findViewById(R.id.btnequal);
+        btnclear = findViewById(R.id.btnclear);
+    }
+
+    public void clickListener() {
+        btn0.setOnClickListener(this);
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
+        btn6.setOnClickListener(this);
+        btn7.setOnClickListener(this);
+        btn8.setOnClickListener(this);
+        btn9.setOnClickListener(this);
+        btnsum.setOnClickListener(this);
+        btnsub.setOnClickListener(this);
+        btnmulti.setOnClickListener(this);
+        btndiv.setOnClickListener(this);
+        btnper.setOnClickListener(this);
+        btnequal.setOnClickListener(this);
+        btnclear.setOnClickListener(this);
+    }
 }
